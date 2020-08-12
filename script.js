b4fcb0c438027b9974723f6c30eb2cbd2656262e
Malva37 <irynapysanko@gmail.com>
2020-08-12 11:40:12 +0300

    add functional change class to menuLink

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let menuLink = document.querySelectorAll('.menuLink a');
 
 menuLink.forEach(elem => {
-    elem.className = '';
     elem.onclick = () => {
         menuLink.forEach(elem => {
             elem.className = '';
